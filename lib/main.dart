b24f0a5998989359cf6d815d40ce7c5a61f607bd
Silver_Hsiao <silver041692@gmail.com>
2022-11-21 03:23:35 +0800

    完成PopupMenuButton和DropdownButton

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var textEditController = TextEditingController();
+
     var column = Column(
       children: [
         TextField(
@@ -23,6 +24,50 @@ class _MyAppState extends State<MyApp> {
             ScaffoldMessenger.of(context).showSnackBar(snackBar);
           },
           child: const Text('Print'),
+        ),
+        PopupMenuButton(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            color: Colors.white,
+            onSelected: (value) {
+              var snackBar = SnackBar(content: Text(value));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+            itemBuilder: (context) {
+          return [
+            const PopupMenuItem(
+              value: 'Item 1',
+              child: Text('Item 1'),
+            ),
+            const PopupMenuItem(
+              value: 'Item 2',
+              child: Text('Item 2'),
+            ),
+            const PopupMenuItem(
+              value: 'Item 3',
+              child: Text('Item 3'),
+            ),
+          ];
+        }),
+        DropdownButton(
+          items: const [
+            DropdownMenuItem(
+              value: 'Item 1',
+              child: Text('Item 1'),
+            ),
+            DropdownMenuItem(
+              value: 'Item 2',
+              child: Text('Item 2'),
+            ),
+            DropdownMenuItem(
+              value: 'Item 3',
+              child: Text('Item 3'),
+            ),
+          ],
+          onChanged: (value) => setState(() {
+            var snackBar = SnackBar(content: Text(value!));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }),
         ),
       ],
     );
@@ -39,34 +84,3 @@ void main() {
     ),
   ));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
